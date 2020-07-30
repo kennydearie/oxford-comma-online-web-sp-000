c94791282761c2a7b0dcb38ea965458a5fe09080
Nkenge Simms <simmsn@yahoo.com>
2020-07-30 01:55:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     return array.join 
   elsif array.length == 2 
     return array.join( " and ") 
-  elsif array.length > 3 
+  elsif array.length >= 3 
     return  array[0..-2].join(", ") + ", and " + array[-1]
  # else array.length == 4
  #   return array[0..-2].join(", ") + ", and " + array[-1]
